a5b69ad87ebb1d5c129b953734c12c322bd3710a
HDCharles <39544797+HDCharles@users.noreply.github.com>
2022-01-12 23:53:52 -0500

    1) QAT changed to use similar logic to ISTA 2) ISTA updated to operate correctly 3) ISTA updated to increase step size if model doesn't updated
    note: I think its possible that the bigger problem is cycling, which
    this is unlikely to fix. May need some kind of momentum plus adaptive
    step size to handle that

--- optimizer.py
+++ optimizer.py
@@ -6,20 +6,20 @@ from torch.nn import functional as F
 
 
 def adam(
-        params: List[Tensor],
-        grads: List[Tensor],
-        exp_avgs: List[Tensor],
-        exp_avg_sqs: List[Tensor],
-        max_exp_avg_sqs: List[Tensor],
-        state_steps: List[int],
-        amsgrad: bool,
-        beta1: float,
-        beta2: float,
-        lr: float,
-        weight_decay: float,
-        eps: float,
+    params: List[Tensor],
+    grads: List[Tensor],
+    exp_avgs: List[Tensor],
+    exp_avg_sqs: List[Tensor],
+    max_exp_avg_sqs: List[Tensor],
+    state_steps: List[int],
+    amsgrad: bool,
+    beta1: float,
+    beta2: float,
+    lr: float,
+    weight_decay: float,
+    eps: float,
 ):
-    '''Implementation of the Adam Optimization'''
+    """Implementation of the Adam Optimization"""
     with torch.no_grad():
         for i, param in enumerate(params):
 
@@ -41,7 +41,9 @@ def adam(
                 # Maintains the maximum of all 2nd moment running avg. till now
                 torch.maximum(max_exp_avg_sqs[i], exp_avg_sq, out=max_exp_avg_sqs[i])
                 # Use the max. for normalizing running avg. of gradient
-                denom = (max_exp_avg_sqs[i].sqrt() / math.sqrt(bias_correction2)).add_(eps)
+                denom = (max_exp_avg_sqs[i].sqrt() / math.sqrt(bias_correction2)).add_(
+                    eps
+                )
             else:
                 denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
 
@@ -51,13 +53,9 @@ def adam(
 
 
 def ini_alg(
-        params: List[Tensor],
-        grads: List[Tensor],
-        L: float,
-        lam: float,
-        epsilon: float
+    params: List[Tensor], grads: List[Tensor], L: float, lam: float, epsilon: float
 ):
-    '''Implementation of the Algorithm 2 in the paper'''
+    """Implementation of the Algorithm 2 in the paper"""
     with torch.no_grad():
         lr = 1 / L
         alpha = lam * epsilon / L
@@ -70,8 +68,8 @@ def ini_alg(
 
 
 class ISTA(torch.optim.Optimizer):
-    '''Implementation of the proposed approach in which we first perform the Algorithm 2 to update the weights or params in our case,
-    and then, perform the Adam algorithm on the updated point.'''
+    """Implementation of the proposed approach in which we first perform the Algorithm 2 to update the weights or params in our case,
+    and then, perform the Adam algorithm on the updated point."""
 
     def __init__(self, parameters, defaults):
         super(ISTA, self).__init__(parameters, defaults)
@@ -79,8 +77,8 @@ class ISTA(torch.optim.Optimizer):
     def __setstate__(self, state):
         super(ISTA, self).__setstate__(state)
 
-    def step_(self, model, x, y, closure=None):
-        '''the function performs a step of the optimization'''
+    def step_(self, model, x, y, closure=None, multiplier=1):
+        """the function performs a step of the optimization"""
         loss = None
         if closure is not None:
             with torch.enable_grad():
@@ -94,38 +92,38 @@ class ISTA(torch.optim.Optimizer):
             state_sums = []
             max_exp_avg_sqs = []
             state_steps = []
-            epsilon = group['epsilon']
-            L = group['L']
-            lam = group['lam']
+            epsilon = group["epsilon"]
+            L = group["L"] / multiplier
+            lam = group["lam"]
 
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
 
                 params_with_grad.append(p)
                 grads.append(p.grad)
-                state = self.state[p]  # by default we will have a stat:dict that records the current status of the associated parameters.
+                state = self.state[
+                    p
+                ]  # by default we will have a stat:dict that records the current status of the associated parameters.
 
                 if len(state) == 0:
-                    state['step'] = 0
+                    state["step"] = 0
                     # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state["exp_avg"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
                     # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state["exp_avg_sq"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
 
-                exp_avgs.append(state['exp_avg'])
-                exp_avg_sqs.append(state['exp_avg_sq'])
+                exp_avgs.append(state["exp_avg"])
+                exp_avg_sqs.append(state["exp_avg_sq"])
 
-                state['step'] += 1
-                state_steps.append(state['step'])
+                state["step"] += 1
+                state_steps.append(state["step"])
 
-            ini_alg(
-                params_with_grad,
-                grads,
-                L,
-                lam,
-                epsilon
-            )
+            ini_alg(params_with_grad, grads, L, lam, epsilon)
 
             self.zero_grad()
             y_hat = model(x)
@@ -147,3 +145,70 @@ class ISTA(torch.optim.Optimizer):
                 1e-8,
             )
         return loss
+
+
+class QAT(torch.optim.Optimizer):
+    """Implementation of QAT"""
+
+    def __init__(self, parameters, defaults):
+        super(QAT, self).__init__(parameters, defaults)
+
+    def __setstate__(self, state):
+        super(QAT, self).__setstate__(state)
+
+    def step_(self):
+        """the function performs a step of the optimization"""
+        for group in self.param_groups:  # iterate through the parameter groups
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            state_sums = []
+            max_exp_avg_sqs = []
+            state_steps = []
+            epsilon = group["epsilon"]
+            L = group["L"]
+            lam = group["lam"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                params_with_grad.append(p)
+                grads.append(p.grad)
+                state = self.state[
+                    p
+                ]  # by default we will have a stat:dict that records the current status of the associated parameters.
+
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
+
+                exp_avgs.append(state["exp_avg"])
+                exp_avg_sqs.append(state["exp_avg_sq"])
+
+                state["step"] += 1
+                state_steps.append(state["step"])
+
+            adam(
+                params_with_grad,
+                grads,
+                exp_avgs,
+                exp_avg_sqs,
+                max_exp_avg_sqs,
+                state_steps,
+                False,
+                0.9,
+                0.999,
+                1 / L,
+                0.0,
+                1e-8,
+            )
+        return
